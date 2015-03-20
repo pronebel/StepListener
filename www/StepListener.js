5982dffe3e488c7e081df912fc32f95f0c7b251f
nebel <nebel08@gmail.com>
2015-03-20 15:39:27 +0800

    111

---
+++ www/StepListener.js
@@ -0,0 +1,12 @@
+
+
+var argscheck = require('cordova/argscheck'),
+    exec = require('cordova/exec'),
+    utils = require('cordova/utils'),
+
+    timers = {},
+    stepcounter = {
+       
+    };
+
+module.exports = stepcounter;
